f9f6f1fc93a45fc743faf28217484ece95b5ead6
Jimmi Pisalita <mig.jimmi@gmail.com>
2021-04-12 09:11:29 +0200

    initial commit

---
+++ src/main/java/kea/gruppe6/miniprojekt/ServletInitializer.java
@@ -0,0 +1,13 @@
+package kea.gruppe6.miniprojekt;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(MiniprojektApplication.class);
+    }
+
+}
