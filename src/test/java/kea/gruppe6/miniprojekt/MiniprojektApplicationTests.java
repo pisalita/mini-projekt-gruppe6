d80fcd4c8b903877b9a292b8c8f66f9b7b07e158
Jimmi Pisalita <mig.jimmi@gmail.com>
2021-04-12 09:11:29 +0200

    initial commit

---
+++ src/test/java/kea/gruppe6/miniprojekt/MiniprojektApplicationTests.java
@@ -0,0 +1,13 @@
+package kea.gruppe6.miniprojekt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniprojektApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
