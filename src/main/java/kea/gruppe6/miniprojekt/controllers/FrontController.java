78766091710a2ed7abe662b7be631f5d0e6099fe
Jonas Gehrke <jonasegehrke@gmail.com>
2021-04-13 14:41:45 +0200

    removed unused imports

--- src/main/java/kea/gruppe6/miniprojekt/controllers/FrontController.java
+++ src/main/java/kea/gruppe6/miniprojekt/controllers/FrontController.java
@@ -1,6 +1,5 @@
 package kea.gruppe6.miniprojekt.controllers;
 
-import kea.gruppe6.miniprojekt.data.DBManager;
 import kea.gruppe6.miniprojekt.data.UserImpl;
 import kea.gruppe6.miniprojekt.domain.LoginControl;
 import kea.gruppe6.miniprojekt.domain.LoginWishLinkException;
@@ -8,14 +7,9 @@ import kea.gruppe6.miniprojekt.domain.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+
 import org.springframework.web.context.request.WebRequest;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
 
 @Controller
 public class FrontController {
