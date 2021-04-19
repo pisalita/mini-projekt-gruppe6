aecd512839305af381b746d8db09a492dc57a283
Jimmi Pisalita <mig.jimmi@gmail.com>
2021-04-19 10:56:48 +0200

    javaDoc

--- src/main/java/kea/gruppe6/miniprojekt/domain/User.java
+++ src/main/java/kea/gruppe6/miniprojekt/domain/User.java
@@ -10,48 +10,95 @@ public class User {
     private String pwd;
     private boolean loggedIn = false;
 
+    /**
+     * Constructor instantiating a user with email, password and username
+     * @param email email of user
+     * @param pwd password of user
+     * @param username username of user
+     */
+
     public User(String email, String pwd, String username){
         this.email = email;
         this.pwd = pwd;
         this.username = username;
     }
 
+    /**
+     * Checks if the user is logged in by returning boolean
+     * @return returns true if user is logged in, else returns false
+     */
+
     public boolean isLoggedIn(){
         return loggedIn;
     }
 
+    /**
+     * Set user to be logged in after succesfull login attempt or create account
+     * @param loggedIn is true if user is logged in, else false
+     */
 
     public void setLoggedIn(boolean loggedIn){
         this.loggedIn = loggedIn;
     }
 
+    /**
+     * Gets the id of a user
+     * @return returns the id of a user
+     */
+
     public int getId() {
         return id;
     }
+
+    /**
+     * Sets the id of a user
+     * @param id contains the id of a user
+     */
 
     public void setId(int id) {
         this.id = id;
     }
 
+    /**
+     * Gets the email of a user
+     * @return returns email of a user
+     */
+
     public String getEmail() {
         return email;
     }
+
+    /**
+     * Sets the email of a user
+     * @param email contains the email of a user
+     */
 
     public void setEmail(String email) {
         this.email = email;
     }
 
+    /**
+     * Gets the username of a user
+     * @return returns username of a user
+     */
+
     public String getUsername() {
         return username;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
+    /**
+     * Gets the password of a user
+     * @return returns password of a user
+     */
 
     public String getPwd() {
         return pwd;
     }
+
+    /**
+     * Sets the password of a user
+     * @param pwd contains the password of a user
+     */
 
     public void setPwd(String pwd) {
         this.pwd = pwd;
--- src/main/java/kea/gruppe6/miniprojekt/domain/Wish.java
+++ src/main/java/kea/gruppe6/miniprojekt/domain/Wish.java
@@ -9,6 +9,15 @@ public class Wish {
     private boolean reserved;
     private String email;
 
+    /**
+     * Constructor instantiating a wish with title, link, comment, reserved and email
+     * @param title title of wish
+     * @param link link to a wish
+     * @param cmnt comment for a wish
+     * @param reserved is true or false based on if the wish is reserved by others or not
+     * @param email the specific email of the user who creates the wish
+     */
+
     public Wish(String title, String link, String cmnt, boolean reserved, String email) {
         this.title = title;
         this.link = link;
@@ -17,53 +26,55 @@ public class Wish {
         this.email = email;
     }
 
+    /**
+     * Gets the id of a wish
+     * @return returns the wish id
+     */
+
     public int getId() {
         return id;
     }
+
+    /**
+     * Sets the id of a wish
+     * @param id contains the wish id
+     */
 
     public void setId(int id) {
         this.id = id;
     }
 
+    /**
+     * Gets the title of a wish
+     * @return returns the wish title
+     */
+
     public String getTitle() {
         return title;
     }
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getLink() {
-        return link;
-    }
-
-    public void setLink(String link) {
-        this.link = link;
-    }
-
-    public String getCmnt() {
-        return cmnt;
-    }
-
-    public void setCmnt(String cmnt) {
-        this.cmnt = cmnt;
-    }
-
-    public boolean isReserved() {
-        return reserved;
-    }
-
-    public void setReserved(boolean reserved) {
-        this.reserved = reserved;
-    }
+    /**
+     * Gets the email of the user who created the wish
+     * @return returns the specific email of the user who created the wish
+     */
 
     public String getEmail() {
         return email;
     }
 
+    /**
+     * Sets the email of the user who created the wish
+     * @param email contains the specific email of the user who created the wish
+     */
+
     public void setEmail(String email) {
         this.email = email;
     }
+
+    /**
+     * toString method used for testing purposes
+     * @return returns a string that is formatted in a specific way
+     */
 
     @Override
     public String toString() {
