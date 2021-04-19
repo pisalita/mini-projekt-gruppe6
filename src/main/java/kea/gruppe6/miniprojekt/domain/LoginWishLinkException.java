860b75bc5ad3ac08fadf9fb790b0e6a8197c76f8
Jonas Gehrke <jonasegehrke@gmail.com>
2021-04-19 10:54:41 +0200

    Added Javadoc for some classes

--- src/main/java/kea/gruppe6/miniprojekt/domain/LoginWishLinkException.java
+++ src/main/java/kea/gruppe6/miniprojekt/domain/LoginWishLinkException.java
@@ -1,6 +1,11 @@
 package kea.gruppe6.miniprojekt.domain;
 
     public class LoginWishLinkException extends Exception {
+
+        /**
+         * Passes exception message
+         * @param msg the exception message
+         */
         public LoginWishLinkException(String msg) {
             super(msg);
         }
